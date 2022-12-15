7e83bc37b540accc2ec5e34a86c1b069e0b6a5ac
ezel <egoistheresy@outlook.com>
2022-12-14 22:51:54 -0600

    things to be chosen done

--- sketch.js
+++ sketch.js
@@ -4,17 +4,23 @@
 let car;
 let aim;
 let road;
+let building1;
+
+
 // groups
 let zombie_type1_group;
 let zombie_type2_group;
 let zombie_type3_group;
+let blocker_group;
+let bullet_group;
+
+
 
 // images array
 let car_imgs;
 let rank_imgs;
 let healthbar_imgs;
 let background_imgs;
-let building_imgs;
 let roadsign_imgs;
 let explosion_imgs;
 
@@ -26,7 +32,7 @@ let spinning_moon_img;
 let road_img;
 let aim_img;
 let bullet_img;
-
+let blocker_img;
 
 // some constant arrays
 const CAR_ANI_ARR = ["carAnimation0", "carAnimation1", "carAnimation2", "carAnimation3", "carAnimation4"];
@@ -37,11 +43,13 @@ const CANVAS_HEIGHT = 800;
 const CANVAS_WIDTH  = 800;
 const MARGIN = 40;
 
+// some tracking information
+let score;
 
 
 
 
-// preload the game resources
+// reload the game resources
 preload = function(){
   // preload car animation
   car_imgs = new Array();
@@ -69,14 +77,24 @@ preload = function(){
   aim_img = loadAnimation("./assets/props/aim.png", {size: [64,64], frames: 12});
 
   // preload the bullet image
-  bullet_img = loadAnimation("./assets/car/bullet1.png", {size:[32, 32], frames:6});
+   bullet_img = loadAnimation("./assets/car/bullet1.png", {size:[32, 32], frames:6});
+  
+  // preload the building image
+  building_img = loadAnimation("./assets/props/building1.png",{size:[300, 800], frames:1});
+
+
+  // preload the blockers
+  blocker_img = loadAnimation("./assets/props/blocker.png", {size:[64, 64], frames:1});
 }
 
 
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  
+  // set up some tracking information
+  score = 0;
+
+
   // set up the car sprite
   car = new Sprite();
   car.addAni(CAR_ANI_ARR[2], car_imgs[2]); // car fire missle animation
@@ -107,8 +125,30 @@ function setup() {
     aim.position.x = mouse.x;
     aim.position.y = mouse.y;
   }
+
+  // set up the building
+  building1 = new Sprite();
+  building1.addAni("building", building_img);
+  building1.x = CANVAS_WIDTH / 16 * 3;
+  building1.y = CANVAS_HEIGHT / 2;
+  building1.removeColliders();
+  building1.layer = 0;
+
+  // set up the blocker
+  // remove everything in the group
+  blocker_group = new Group();
+  blocker_group.removeAll();
+  bullet_group = new Group();
+  bullet_group.removeAll(); 
+
+
+
+	
 }
 
+function collect(car, gem) {
+	gem.remove();
+}
 
 function draw() {
   // deduct the health for the monster
@@ -116,14 +156,55 @@ function draw() {
   carControl();
   out_of_road();
   out_of_boundary();
+  
+  // place to have the colliders
+
+  bullet_group.collides(blocker_group, bulletHit);
+  // car.collides(blocker_group, carHit);
+  car.collides(blocker_group, collect);
+  // create two blockers if situation satisfies
+  
+  
+  if (blocker_group.size() < 2){
+    createBlocker(random(300, 500), 0, 5, 120);
+  }
+
+
+
+  // console.log(bullet_group.size());
+  // console.log(blocker_group.size());
 }
 
+function carHit(car, sprite){
+  car.remove();
+  sprite.remove();
+}
+
+function bulletHit(bullet, sprite){
+  bullet.remove();
+  sprite.remove();
+}
+
+
+
+// create a blocker object
+function createBlocker(x, y, speed = 5){
+  let blocker = new Sprite();
+  blocker.addAni("blockerani", blocker_img);
+  blocker.x = x;
+  blocker.y = y;
+  // blocker.kinematic = true;
+  blocker.speed = speed;
+  blocker.direction = 90;
+
+  blocker_group.add(blocker);
+}
 
 // control the movement of the car
 function carControl(){ 
 
 
-  if(!car.removed && kb.presses('s')){
+  if(!car.removed && kb.presses('q')){
     createBullet(12, 30, bullet_img);
     car.changeAni(CAR_ANI_ARR[1]);
     createExplosion(car.x, car.y, 3, 10, 1);
@@ -131,12 +212,19 @@ function carControl(){
     car.changeAni(CAR_ANI_ARR[0]);
   }
 
-  if (kb.presses('left') || kb.presses('D')) {
-      car.move(50, 'left', 4);
+  if (kb.presses('down') || kb.presses('S') || kb.pressing('down') || kb.pressing('S')) {
+      car.move(50, 'down', 3);
+  }
+  if (kb.presses('up') || kb.presses('W') || kb.pressing('up') || kb.pressing('W')) {
+      car.move(50, 'up', 3);
   }
 
-  if (kb.presses('right') || kb.presses('D')) {
-   car.move(50, 'right', 4);
+  if (kb.presses('left') || kb.presses('A') || kb.pressing('left') || kb.pressing('A')) {
+      car.move(50, 'left', 3);
+  }
+
+  if (kb.presses('right') || kb.presses('D') || kb.pressing('right') || kb.pressing('D')) {
+   car.move(50, 'right', 3);
   }
 
   if(kb.presses('k')){
@@ -145,7 +233,6 @@ function carControl(){
 }
 
 function out_of_boundary() {
-
   for (var i = 0; i < allSprites.length; i++) {
     let s = allSprites[i];
     if (s.position.x < -MARGIN) {
@@ -153,7 +240,6 @@ function out_of_boundary() {
     } else if (s.position.x > width + MARGIN) {
       s.position.x = -MARGIN;
     }
-
     if (s.position.y < -MARGIN) {
       s.position.y = height + MARGIN;
     } else if (s.position.y > height + MARGIN) {
@@ -182,7 +268,7 @@ function createExplosion(x, y, type, life, scale){
 }
 
 /**
- * Create Bullet
+ * Create a single Bullet
  * @param { } speed 
  * @param { } life 
  * @param {*} animation 
@@ -206,4 +292,6 @@ function createBullet(speed = 13, life, animation){
   
   bullet.speed = speed;
   bullet.kinematic = true;
+  bullet_group.add(bullet);
+
 }
